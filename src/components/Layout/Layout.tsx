4147af9b06c45a68a6a14ef4711054009e0a1e3d
Mathieu Marques <mathieumarques78@gmail.com>
2021-07-07 01:30:24 +0200

    fix(layout): fix layout offset

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,23 @@
-import React, { FunctionComponent, ReactNode } from 'react';
+import React, { FunctionComponent, ReactChild } from 'react';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import { Theme, makeStyles } from '@material-ui/core/styles';
 import { Footer } from '@/components/Footer/Footer';
 import { Header } from '@/components/Header/Header';
 import { Meta } from '@/components/Meta/Meta';
 
 const useStyles = makeStyles((theme: Theme) => ({
-  offset: theme.mixins.toolbar,
+  offset: {
+    ...theme.mixins.toolbar,
+    marginBottom: theme.spacing(3),
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: theme.spacing(2),
+    },
+  },
 }));
 
 interface Props {
-  children?: ReactNode;
+  children: ReactChild;
 }
 
 export const Layout: FunctionComponent<Props> = ({ children }) => {
